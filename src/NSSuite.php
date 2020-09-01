f3bf6236cf8da16554ae57eeaaac11e7929e302a
Matheus Mazzoni <54732019+matheusmazzoni@users.noreply.github.com>
2020-09-01 11:22:30 -0300

    Update NSSuite.php

--- src/NSSuite.php
+++ src/NSSuite.php
@@ -680,7 +680,7 @@ class NSSuite{
 
                     if ($statusDownload != 200) $motivo = $resposta['motivo'];
                 }else{
-                    $motivo = $resposta['motivo'];
+                    $motivo = $resposta['xMotivo'];
                 }
             }else if ($statusConsulta == -2) {
 
